4581ffacc11a894741411b1d0f7bbf24a8c9e1d2
Vyacheslav <dyaditsyn@gmail.com>
2021-03-16 22:11:57 +0200

    picture and title replacement within 5 sec

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 // script
+
 function submitForm(){
     document.getElementById('myform').classList.add('form-submitted');
  } 
 
+
 const images = ["img/baner1.jpg","img/baner2.jpg","img/baner3.jpg"];
-let imgIndex = 0;
+let imgIndex = 1;
 
 function changeImage() {
-    document.getElementById("img").src = images[imgIndex];
+    document.getElementById('img').src = images[imgIndex];
     if (imgIndex == images.length-1) {
       imgIndex = 0;
     } else {
       imgIndex++;
     }
-
-    setInterval("changeImage()", 5000);
 }
 
 
-// const titles = ["yellow click&pick;","שטחי מסחר פז סנטר","טיפז"];
-// let titleIndex = 0;
-// const title = document.getElementById("title");
+const titles = ["yellow click&pick;","שטחי מסחר פז סנטר","טיפז"];
+let titleIndex = 1;
+const element = document.getElementById('title');
 
-// function changeTitle() {
-//     title.innerText = titles[titleIndex];
-//     if (titleIndex == titles.length-1) {
-//         titleIndex = 0;
-//     } else {
-//         titleIndex++;
-//     }
+function changeTitle() {
+    element.innerHTML = titles[titleIndex];
+    if (titleIndex == titles.length-1) {
+        titleIndex = 0;
+    } else {
+        titleIndex++;
+    }
+}
 
-//     setInterval("changeTitle()", 5000);
-// }
+setInterval(function() {
 
+    changeTitle();
+    changeImage();
+}, 5000);
